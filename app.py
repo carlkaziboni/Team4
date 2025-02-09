095761f1be48c33c39470cc6478eb4ff05e329ad
wryzxec <lucas.bruckbauer@gmail.com>
2025-02-09 00:57:30 +0000

    time to sleep...

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import os
 
 from flask import Flask, render_template, request, jsonify
 from graphgen import plot
-
+from agi import analyse
 
 app = Flask(__name__)
 
@@ -16,8 +17,20 @@ def index():
 @app.route('/update_text', methods=['POST'])
 def update_text():
     global current_text
-    new_text = request.form['text']
+
+    data = request.json
+    img_src = data.get('largeImage')
+
+    if img_src.startswith("http://") or img_src.startswith("https://"):
+        img_filename = os.path.basename(img_src)  # Extract filename (e.g., 'graph.png')
+        image_path = os.path.join("static", img_filename)  # Convert to local path
+    else:
+        image_path = img_src  # Already a valid local path
+
+    new_text = analyse(image_path)
+    print(new_text)
     current_text = new_text
+    print(current_text)
     return jsonify({"status": "success", "new_text": current_text})
 
 @app.route('/submit_dates', methods=['POST'])
